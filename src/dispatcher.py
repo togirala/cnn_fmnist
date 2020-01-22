d99848ff3c170320a93d2bba8b81cae627cd7f09
togirala <teja90.exe@gmail.com>
2020-01-22 03:21:43 +0000

    ver 1.0

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -8,7 +8,7 @@ res_path = 'models/'
 
 
 input_training_set = ds.FashionMNIST(
-        root='input/', \
+        root='input/', 
         train = True, 
         # transform=None, 
         target_transform=None, 
@@ -17,10 +17,18 @@ input_training_set = ds.FashionMNIST(
         )
 
 # data_loader = data.DataLoader(input_training_set, batch_size=4, shuffle=True, num_workers=args.nThreads)
-batch_loader = data.DataLoader(input_training_set, batch_size=1000, shuffle=False)
+batch_loader = data.DataLoader(input_training_set, batch_size=1000, shuffle=False, num_workers=1)
 
 
 tb_status = True
+
+
+parameters = dict(
+        lr = [0.01, 0.001],
+        bs = [10, 100, 1000]#,
+        #shuffle = [True, False]
+        )
+        
 
 
 
--- src/train.py
+++ src/train.py
@@ -1,11 +1,11 @@
-import dispatcher
+from dispatcher import batch_loader, tb_status, res_path, parameters
 import cnn1
 import torch.nn.functional as F
 import torch.optim as optim
 import torch
 from torch.utils.tensorboard import SummaryWriter
 import torchvision
-
+from itertools import product
 
 
 
@@ -18,53 +18,64 @@ def correct_preds(preds, labels):
 
 def train():
 
-    batch_loader = dispatcher.batch_loader
-    images, labels = next(iter(batch_loader))
+    param_values = [v for v in parameters.values()]
+    
+    for lr, bs in product(*param_values):
+        images, labels = next(iter(batch_loader))
 
-    model = cnn1.CNN1()
-    optimizer = optim.Adam(model.parameters(), lr=0.003)
+        model = cnn1.CNN1()
+        optimizer = optim.Adam(model.parameters(), lr=lr)
+    
 
-    if dispatcher.tb_status:
-        tb = SummaryWriter()
-        grid = torchvision.utils.make_grid(images)
-        tb.add_image('image', grid)
-        tb.add_graph(model, images)
+
+        if tb_status:
+        
+            comment = f'Batch size={bs}  Learning rate={lr}'
+            tb = SummaryWriter(comment=comment)
+
+            grid = torchvision.utils.make_grid(images)
+            tb.add_image('image', grid)
+            tb.add_graph(model, images)
+
+        
+    
+        for epoch in range(5):
+
+            total_loss = 0
+            total_correct = 0
+
+            for batch in batch_loader:
+                images, labels = batch
+        
+                preds = model(images) # forward prop ==> get predictions
+                loss = F.cross_entropy(preds, labels) # calculate loss
+                optimizer.zero_grad() # zero the grads
+                loss.backward() # backward prop ==> calculate gradients
+                optimizer.step() # update weights
+        
+                total_loss += loss.item() * bs
+                total_correct += correct_preds(preds, labels)
+
+            print(f"epoch: {epoch} ==> total correct: {total_correct} and total loss: {total_loss}")
+        
+            if tb_status:
+                tb.add_scalar('Loss', total_loss, epoch)
+                tb.add_scalar('Total correct', total_correct, epoch)
+
+                for name, weight in model.named_parameters():
+                    tb.add_histogram(name, weight, epoch)
+                    tb.add_histogram(f'{name}.grad', weight.grad, epoch)
+
+
+        print(f"Learning Rate: {lr} and Batch Size: {bs} ==> total correct: {total_correct} and total loss: {total_loss}")
+        
+        
+        if tb_status:
+            tb.close()
+
 
     
-    for epoch in range(10):
-
-        total_loss = 0
-        total_correct = 0
-
-        for batch in batch_loader:
-            images, labels = batch
-        
-            preds = model(images) # forward prop ==> get predictions
-            loss = F.cross_entropy(preds, labels) # calculate loss
-            optimizer.zero_grad() # zero the grads
-            loss.backward() # backward prop ==> calculate gradients
-            optimizer.step() # update weights
-        
-            total_loss += loss.item()
-            total_correct += correct_preds(preds, labels)
-
-        print(f"epoch: {epoch} ==> total correct: {total_correct} and total loss: {total_loss}")
-        
-        if dispatcher.tb_status:
-            tb.add_scalar('Loss', total_loss, epoch)
-            tb.add_scalar('Total correct', total_correct, epoch)
-
-            tb.add_histogram('conv1.bias', model.conv1.bias, epoch)
-            tb.add_histogram('conv1.weight', model.conv1.weight, epoch)
-            tb.add_histogram('conv1.weight.grad', model.conv1.weight.grad, epoch)
-
-
-    if dispatcher.tb_status:
-        tb.close()
-
-
-    
-    torch.save(model, f'{dispatcher.res_path}cnn1.pt')
+    torch.save(model, f'{res_path}cnn1.pt')
     #return model
 
 
