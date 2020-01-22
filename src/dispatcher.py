a139d4e670c554830617f9ba13b7db0f2c5912bc
togirala <teja90.exe@gmail.com>
2020-01-22 22:52:03 +0000

    working on dispatcher class

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -1,13 +1,52 @@
 import torchvision.datasets as ds
 import torchvision.transforms as transforms
-import torch.utils.data as data
+import torch.utils.data 
 
 
 
-res_path = 'models/'
+class Dispatcher():
 
 
-input_training_set = ds.FashionMNIST(
+    def __init__(self):
+        self.res_path = None
+        self.training_set = None
+        self.test_set = None
+        self.parameters = None
+
+
+    def get_parameters():
+        
+        parameters = dict(
+                lr = []
+                ,bs = []
+                ,shuffle = []
+                )
+
+
+        return parameters
+        
+
+    def get_data_batch(data_set):
+        batch_data = torch.utils.data.DataLoader(
+                data_set
+                ,batch_size = 1000
+                ,shuffle = False
+                ,num_workers = 1
+                )
+    
+        return batch_data
+
+
+    
+
+
+
+
+
+    res_path = 'models/'
+
+
+    input_training_set = ds.FashionMNIST(
         root='input/', 
         train = True, 
         # transform=None, 
@@ -16,19 +55,19 @@ input_training_set = ds.FashionMNIST(
         transform = transforms.Compose([transforms.ToTensor()])
         )
 
-# data_loader = data.DataLoader(input_training_set, batch_size=4, shuffle=True, num_workers=args.nThreads)
-batch_loader = data.DataLoader(input_training_set, batch_size=1000, shuffle=False, num_workers=1)
+        # data_loader = data.DataLoader(input_training_set, batch_size=4, shuffle=True, num_workers=args.nThreads)
+    batch_loader = data.DataLoader(input_training_set, batch_size=1000, shuffle=False, num_workers=1)
 
 
-tb_status = True
+    tb_status = True
 
 
-parameters = dict(
-        lr = [0.01, 0.001],
-        bs = [10, 100, 1000]#,
-        #shuffle = [True, False]
-        )
-        
+    parameters = dict(
+            lr = [0.01, 0.001],
+            bs = [10, 100, 1000]#,
+            #shuffle = [True, False]
+            )
+            
 
 
 
