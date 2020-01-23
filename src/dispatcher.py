ad1f0e26c661508a9e9a2b360c1a2b835b9f600a
togirala <teja90.exe@gmail.com>
2020-01-23 01:51:59 +0000

    test run 1

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -1,6 +1,10 @@
 import torchvision.datasets as ds
 import torchvision.transforms as transforms
 import torch.utils.data 
+from collections import OrderedDict
+from collections import namedtuple
+from itertools import product
+import cnn1
 
 
 
@@ -12,61 +16,66 @@ class Dispatcher():
         self.training_set = None
         self.test_set = None
         self.parameters = None
+        self.model = None
+    
+    def get_model(self):
+
+        return cnn1.CNN1()
 
 
-    def get_parameters():
+    def get_parameters(self):
         
-        parameters = dict(
-                lr = []
-                ,bs = []
-                ,shuffle = []
+        parameters = OrderedDict(
+                lr = [0.01, 0.001]
+                ,bs = [100, 1000]
+                # ,shuffle = []
                 )
-
 
         return parameters
         
 
-    def get_data_batch(data_set):
+    def get_dataset(self):
+        self.training_set = ds.FashinMNIST(
+                root = 'input/',
+                train = True,
+                download = True,
+                transform = transforms.Compose([transforms.ToTensor()])
+                )
+
+
+
+    def get_data_batch(self, batch_size = 256, shuffle = False, num_workers = 1):
         batch_data = torch.utils.data.DataLoader(
-                data_set
-                ,batch_size = 1000
-                ,shuffle = False
-                ,num_workers = 1
+                dataset = get_dataset()
+                batch_size = batch_size,
+                shuffle = shuffle
+                num_workers = num_workers
                 )
     
         return batch_data
 
-
     
+    @staticmethod
+    def get_runs(self):
+        
+        params = get_parameters()
+        run = namedtuple('run', param.keys())
+        runs = list()
+ 
+        for v in product(*params.values()):
+            runs.append(run(*v))
+
+        return runs
 
 
 
 
 
-    res_path = 'models/'
+
+    #res_path = 'models/'
 
 
-    input_training_set = ds.FashionMNIST(
-        root='input/', 
-        train = True, 
-        # transform=None, 
-        target_transform=None, 
-        download=True,
-        transform = transforms.Compose([transforms.ToTensor()])
-        )
 
-        # data_loader = data.DataLoader(input_training_set, batch_size=4, shuffle=True, num_workers=args.nThreads)
-    batch_loader = data.DataLoader(input_training_set, batch_size=1000, shuffle=False, num_workers=1)
-
-
-    tb_status = True
-
-
-    parameters = dict(
-            lr = [0.01, 0.001],
-            bs = [10, 100, 1000]#,
-            #shuffle = [True, False]
-            )
             
 
 
--- src/train.py
+++ src/train.py
@@ -6,73 +6,174 @@ import torch
 from torch.utils.tensorboard import SummaryWriter
 import torchvision
 from itertools import product
+from collections import OrderedDict
+from collections import namedtuple
+
+
+class Run():
+    
+    def __init__(self):
+        
+        self.params = None
+        self.count = 0
+        self.data = list()
+        self.start_time = None
+
+        self.tb = None
+
+        self.data = None
+        self.model = None
+
+
+    def begin(self, run, model, data):
+
+        self.start_time = time.time()
+        
+        self.params = run
+        self.count += 1
+        
+        self.data = data
+        self.model = model
+
+        
+        self.tb = SummmaryWriter(comment = f'-{run}')
+
+        images, labels = next(iter(self.data))
+        grid = torchvision.utils.make_grid(images)
+        self.tb.add_image('images', grid)
+        self.tb.add_graph(self.model, images)
+
+        
+
+    def end(self):
+
+        
+        self.Epoch.count = 0
+        self.tb.close()
+
+
+
+    def save(self, fileName):
+
+        pd.DataFrame.from_dict(Run.data, orient='columns').to_csv(f'{fileNmae}.csv')
+        with open(f'{fileName}.json', 'w', encoding='utf-8') as f:
+            json.dump(Run.data, f, ensure_ascii=False, indent=4)
 
 
 
 
-def correct_preds(preds, labels):
-    return preds.argmax(dim=1).eq(labels).sum().item()
+
+
+    
+
+class Epoch(Run):
+    
+    def __init__(self):
+        self.count = 0
+        self.start_time = None
+        self.loss = 0
+        self.accuracy = 0
+        
+
+    def begin(self):
+
+        self.start_time = time.time()
+        self.count += 1
+        self.loss = 0
+        self.accuracy = 0
+
+
+    def end(self):
+        duration = time.time() - self.start_time
+        
+        loss = self.loss / len(Run.data.dataset)
+        accuracy = self.accuracy / len(Run.data.dataset)
+
+        self.tb.add_scalar('Loss: ', loss, self.count)
+        self.tb.add_scalar('Accuracy: ', accuracy, self.count)
+
+        for name, param in Run.model.named_parameters():
+            self.add_histogram(name, param, self.count)
+            self.add_histogram(f'{name}.grad', param.grad, self.count)
+
+        results = OrderedDict()
+        results['run'] = Run.count
+        results['epoch'] = count
+        results['loss'] = loss
+        results['accuracy'] = accuracy
+        results['epoch duration'] = duration
+        results['run duration'] = Run.duration
+        
+        for k, v in Run.params._asdict().items(): results[k] = v
+
+        Run.data.append(results)
+        df = pd.DataFrame.from_dict(Run.data, orient = 'columns')
+
+        
+    def track_loss(self, loss):
+        self.loss += loss.item() * Run.data.batch_size
+
+    def track_accuracy(self, preds, labels):
+        self.accuracy += self._get_accuracy(preds, labels)
+
+    @torch.no_grad()
+    def _get_accuracy(self, preds, labels):
+        return preds.argmax(dim=1).eq(labels).sum().item()/len(Run.data.dataset)
+
+    def save(self, fileName):
+
+        pd.DataFrame.from_dict(Run.data, orient='columns').to_csv(f'{fileNmae}.csv')
+        with open(f'{fileName}.json', 'w', encoding='utf-8') as f:
+            json.dump(Run.data, f, ensure_ascii=False, indent=4)
+
+
+
+
+
+# def correct_preds(preds, labels):
+#     return preds.argmax(dim=1).eq(labels).sum().item()
 
 
 
 
 def train():
 
-    param_values = [v for v in parameters.values()]
+    # param_values = [v for v in parameters.values()]
     
-    for lr, bs in product(*param_values):
-        images, labels = next(iter(batch_loader))
-
-        model = cnn1.CNN1()
-        optimizer = optim.Adam(model.parameters(), lr=lr)
-    
-
-
-        if tb_status:
+    r = Run()
+    e = Epoch()
+    for run in Dispatcher.get_runs():
         
-            comment = f'Batch size={bs}  Learning rate={lr}'
-            tb = SummaryWriter(comment=comment)
 
-            grid = torchvision.utils.make_grid(images)
-            tb.add_image('image', grid)
-            tb.add_graph(model, images)
 
+        model = dispatcher.get_model()
+        optimizer = optim.Adam(model.parameters(), lr=run.lr)
+        batch_loader = Dispatcher.get_data_batch(batch_size = run.bs)
         
-    
+        r.begin(run, model, batch_loader)
         for epoch in range(5):
-
-            total_loss = 0
-            total_correct = 0
-
+            
+            e.begin()
             for batch in batch_loader:
-                images, labels = batch
-        
+                
+                images = batch[0]
+                labels = batch[1]
+
                 preds = model(images) # forward prop ==> get predictions
                 loss = F.cross_entropy(preds, labels) # calculate loss
                 optimizer.zero_grad() # zero the grads
                 loss.backward() # backward prop ==> calculate gradients
                 optimizer.step() # update weights
         
-                total_loss += loss.item() * bs
-                total_correct += correct_preds(preds, labels)
+                e.track_loss(loss)
+                e.track_accuracy(preds, labels)
 
-            # print(f"epoch: {epoch} ==> total correct: {total_correct} and total loss: {total_loss}")
-        
-            if tb_status:
-                tb.add_scalar('Loss', total_loss, epoch)
-                tb.add_scalar('Total correct', total_correct, epoch)
+                e.end()
 
-                for name, weight in model.named_parameters():
-                    tb.add_histogram(name, weight, epoch)
-                    tb.add_histogram(f'{name}.grad', weight.grad, epoch)
+            r.end()
 
-
-        print(f"Learning Rate: {lr} and Batch Size: {bs} ==> total correct: {total_correct} and total loss: {total_loss}")
-        
-        
-        if tb_status:
-            tb.close()
-
+        r.save('results')
+        print(f'run')
 
     
     torch.save(model, f'{res_path}cnn1.pt')
